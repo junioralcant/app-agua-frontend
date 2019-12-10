1add4d1e7ec79f078181a5f1e50ba593ce6b4035
Laks Castro <lakhsmycastro336@gmail.com>
2019-12-10 10:18:34 -0400

    Update index.js

--- src/pages/Pedidos/index.js
+++ src/pages/Pedidos/index.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import CadPedidos from "../../components/CadProduto";
+
+//Renomeei o componente pra ficar com o mesmo nome da pasta dele, se quiser mudar de novo tem problema não, só mudar
+import CadProduto from "../../components/CadProduto";
 import Sidebar from "../../components/Sidebar";
 
 import { Container } from "./styles";
 
-export default function Pedidos() {
+export default function Pedidos({ ...props }) {
   return (
     <Container>
       <Sidebar />
-      <CadPedidos />
+      <CadProduto { ...props } />
     </Container>
   );
 }
